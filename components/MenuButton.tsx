68aef25af099c37fac3a833f6fd9d3dc8dae0655
Hossein Almasi <h.almasi2012@gmail.com>
2024-10-23 19:21:31 +0330

    change style for active menu links

--- components/MenuButton.tsx
+++ components/MenuButton.tsx
@@ -1,14 +1,22 @@
 import Link from "next/link";
 import { ReactNode } from "react";
-
+import { usePathname } from "next/navigation";
 interface Props {
   children: ReactNode;
   href: string;
 }
 
 export default function MenuButton({ children, href }: Props) {
+  const path = usePathname();
   return (
-    <Link className="border-b border-yellow-500" href={href}>
+    <Link
+      className={`border-b ${
+        (path.startsWith(href) && href !== "/") || path === href
+          ? "border-yellow-800"
+          : "border-yellow-500"
+      }`}
+      href={href}
+    >
       {children}
     </Link>
   );
