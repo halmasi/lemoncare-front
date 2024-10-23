8104239531c4e335fd1b32e599d9201f1f03071f
halmasi <h.almasi2012@gmail.com>
2024-10-23 15:18:16 +0330

    initial structure

---
+++ components/Footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+	return (
+		<>
+			<h2 className='bg-yellow-200 min-h-[calc(20svh)]'>Footer</h2>
+		</>
+	);
+}
---
+++ components/Navbar.tsx
@@ -0,0 +1,7 @@
+export default function Navbar() {
+	return (
+		<>
+			<h2 className='bg-white h-24'>Navbar</h2>
+		</>
+	);
+}
