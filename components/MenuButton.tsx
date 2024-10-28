52b0c0e9597b0e2d8f0fa4bd6df3c9527305a637
Hossein Almasi <h.almasi2012@gmail.com>
2024-10-28 21:23:52 +0330

    fix submenu visibility

--- components/MenuButton.tsx
+++ components/MenuButton.tsx
@@ -1,6 +1,6 @@
 'use client';
 import Link from 'next/link';
-import { ReactNode, useState } from 'react';
+import { ReactNode } from 'react';
 import { usePathname } from 'next/navigation';
 import { FaChevronDown } from 'react-icons/fa';
 
@@ -15,7 +15,7 @@ export default function MenuButton({ children, slug, submenu }: Props) {
 
   return (
     <div
-      className={`flex flex-row bg-white border-b items-center text-lg pb-2 mx-2 md:cursor-pointer ${
+      className={`flex flex-row border-b items-center text-lg pb-2 mx-2 md:cursor-pointer ${
         (path.startsWith(slug) && slug !== '/') || path === slug
           ? 'bg-green-700 rounded-lg text-white md:border-yellow-800 md:text-black md:bg-transparent md:rounded-none'
           : 'border-yellow-500 rounded-none'
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,14 +1,27 @@
 'use client';
 
-import { ReactNode, useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
 import { IoCloseSharp, IoMenu } from 'react-icons/io5';
 import SearchInput from './SearchInput';
 import Logo from '@/public/lemoncareLogoForHeader.png';
+import MenuButton from './MenuButton';
+import { MenuProps } from '@/utils/menu';
 
-export default function Navbar({ menuItems }: { menuItems: ReactNode }) {
+export default function Navbar({
+  menuItems,
+}: {
+  menuItems?: (MenuProps | undefined)[] | undefined;
+}) {
   const [menuState, setMenustate] = useState<boolean>(false);
+  const [subMenuHead, setSubMenuHead] = useState<{
+    title: string;
+    expand: boolean;
+  }>({
+    title: '',
+    expand: false,
+  });
   const [scrollData, setScrollData] = useState({ y: 0, latestY: 0 });
   const [visibility, setVisibility] = useState<boolean>(true);
 
@@ -67,7 +80,45 @@ export default function Navbar({ menuItems }: { menuItems: ReactNode }) {
         <div className={`w-full ${menuState ? 'flex flex-col' : 'hidden'}`}>
           <SearchInput />
           <div className="flex flex-col w-full h-[80svh] space-y-5 overflow-scroll">
-            {menuItems}
+            {menuItems &&
+              menuItems.map((item) => {
+                if (item)
+                  return (
+                    <div
+                      onClick={() => {
+                        setSubMenuHead((prev) => {
+                          return {
+                            title: item.title,
+                            expand: !prev.expand,
+                          };
+                        });
+                      }}
+                      className="flex flex-col group"
+                    >
+                      <MenuButton
+                        key={item.id}
+                        slug={item.slug}
+                        submenu={item.childCategories}
+                      >
+                        {item.title}
+                      </MenuButton>
+                      {item.childCategories.length > 0 && (
+                        <div
+                          className={`bg-gray-200 rounded-lg pr-5 ${subMenuHead.title === item.title && subMenuHead.expand ? 'flex' : 'hidden'}`}
+                        >
+                          {item.childCategories.map((subItem) => (
+                            <MenuButton
+                              submenu={subItem.childCategories}
+                              slug={subItem.slug}
+                            >
+                              {subItem.title}
+                            </MenuButton>
+                          ))}
+                        </div>
+                      )}
+                    </div>
+                  );
+              })}
           </div>
         </div>
       </div>
@@ -82,7 +133,35 @@ export default function Navbar({ menuItems }: { menuItems: ReactNode }) {
               className="h-10 w-auto drop-shadow-lg"
             />
           </Link>
-          {menuItems}
+          {menuItems &&
+            menuItems.map((item) => {
+              if (item)
+                return (
+                  <div className="flex flex-col group">
+                    <MenuButton
+                      key={item.id}
+                      slug={item.slug}
+                      submenu={item.childCategories}
+                    >
+                      {item.title}
+                    </MenuButton>
+                    {item.childCategories.length > 0 && (
+                      <div className="hidden bg-white group-hover:md:block">
+                        <div className="absolute top-auto p-3">
+                          {item.childCategories.map((subItem) => (
+                            <MenuButton
+                              submenu={subItem.childCategories}
+                              slug={subItem.slug}
+                            >
+                              {subItem.title}
+                            </MenuButton>
+                          ))}
+                        </div>
+                      </div>
+                    )}
+                  </div>
+                );
+            })}{' '}
         </div>
         <SearchInput />
       </div>
