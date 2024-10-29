56e23453754bb8fc86ebfe4a3f6615761ee53319
Hossein Almasi <h.almasi2012@gmail.com>
2024-10-29 20:29:42 +0330

    fix not found page size

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link'
 
 export default function NotFound() {
   return (
-    <div className="flex flex-col pt-20 h-screen items-center bg-gradient-to-br from-yellow-500 via-gray-400 to-pink-400 text-center text-white">
+    <div className="flex flex-col pt-20 w-full h-screen items-center bg-gradient-to-br from-yellow-500 via-gray-400 to-pink-400 text-center text-white">
       <h1 className="text-4xl font-bold  mb-6 drop-shadow-lg">
         صفحه‌ای یافت نشد!
       </h1>
