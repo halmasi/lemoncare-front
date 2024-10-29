8a2be30eaa2c5003715bec9f85b8fb4cfedaddf8
Hossein Almasi <h.almasi2012@gmail.com>
2024-10-29 20:22:07 +0330

    fix layout conflict with fullscreen pages

--- app/layout.tsx
+++ app/layout.tsx
@@ -32,7 +32,7 @@ export default function RootLayout({
       >
         <Navbar />
         <div className="flex bg-gray-50 relative z-10 justify-center">
-          <div className="min-h-svh container max-w-screen-xl py-5 px-10">
+          <div className="flex min-h-svh w-full py-5 px-10 justify-center">
             {children}
           </div>
         </div>
