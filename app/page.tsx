0f0dac022d0d29486d410a7c1f4e24bc4e29dce5
Hossein Almasi <h.almasi2012@gmail.com>
2024-11-05 19:45:21 +0330

    design post skeleton

--- app/page.tsx
+++ app/page.tsx
@@ -1,83 +1,14 @@
+import PostsSkeleton from '@/components/Skeleton';
+
 export default function Home() {
   return (
     <main className="container max-w-screen-xl py-5 px-10">
       <h1>متن هدر ۱</h1>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci
-        facilis ipsum iusto iste pariatur, quos obcaecati consectetur assumenda
-        eum quia necessitatibus omnis commodi soluta, recusandae, voluptates a
-        modi magni! Id.
-      </p>
-      <h2>متن هدر ۲</h2>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci
-        facilis ipsum iusto iste pariatur, quos obcaecati consectetur assumenda
-        eum quia necessitatibus omnis commodi soluta, recusandae, voluptates a
-        modi magni! Id.
-      </p>
-      <h3>متن هدر ۳</h3>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci
-        facilis ipsum iusto iste pariatur, quos obcaecati consectetur assumenda
-        eum quia necessitatibus omnis commodi soluta, recusandae, voluptates a
-        modi magni! Id.
-      </p>
-      <h4>متن هدر ۴</h4>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci
-        facilis ipsum iusto iste pariatur, quos obcaecati consectetur assumenda
-        eum quia necessitatibus omnis commodi soluta, recusandae, voluptates a
-        modi magni! Id.
-      </p>
-      <h5>متن هدر ۵</h5>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci
-        facilis ipsum iusto iste pariatur, quos obcaecati consectetur assumenda
-        eum quia necessitatibus omnis commodi soluta, recusandae, voluptates a
-        modi magni! Id.
-      </p>
-      <h6>متن هدر ۶</h6>
-      <p>
-        <strong>Lorem ipsum dolor sit,</strong> amet consectetur adipisicing
-        elit. Numquam magnam unde est maiores voluptas doloribus voluptatum
-        tempore expedita optio a cumque provident officia odit ea recusandae eum
-        quibusdam, maxime cupiditate. Numquam eveniet voluptatum voluptas
-        facilis consectetur, illo nulla cupiditate beatae perferendis, officiis
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Numquam magnam
-        unde est maiores voluptas doloribus voluptatum tempore expedita optio a
-        cumque provident officia odit ea recusandae eum quibusdam, maxime
-        cupiditate. Numquam eveniet voluptatum voluptas facilis consectetur,
-        illo nulla cupiditate beatae perferendis, officiis Lorem ipsum dolor
-        sit, amet consectetur adipisicing elit. Numquam magnam unde est maiores
-        voluptas doloribus voluptatum tempore expedita optio a cumque provident
-        officia odit ea recusandae eum quibusdam, maxime cupiditate. Numquam
-        eveniet voluptatum voluptas facilis consectetur, illo nulla cupiditate
-        beatae perferendis, officiis Lorem ipsum dolor sit, amet consectetur
-        adipisicing elit. Numquam magnam unde est maiores voluptas doloribus
-        voluptatum tempore expedita optio a cumque provident officia odit ea
-        recusandae eum quibusdam, maxime cupiditate. Numquam eveniet voluptatum
-        voluptas facilis consectetur, illo nulla cupiditate beatae perferendis,
-        officiis Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-        Numquam magnam unde est maiores voluptas doloribus voluptatum tempore
-        expedita optio a cumque provident officia odit ea recusandae eum
-        quibusdam, maxime cupiditate. Numquam eveniet voluptatum voluptas
-        facilis consectetur, illo nulla cupiditate beatae perferendis, officiis
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Numquam magnam
-        unde est maiores voluptas doloribus voluptatum tempore expedita optio a
-        cumque provident officia odit ea recusandae eum quibusdam, maxime
-        cupiditate. Numquam eveniet voluptatum voluptas facilis consectetur,
-        illo nulla cupiditate beatae perferendis, officiis Lorem ipsum dolor
-        sit, amet consectetur adipisicing elit. Numquam magnam unde est maiores
-        voluptas doloribus voluptatum tempore expedita optio a cumque provident
-        officia odit ea recusandae eum quibusdam, maxime cupiditate. Numquam
-        eveniet voluptatum voluptas facilis consectetur, illo nulla cupiditate
-        beatae perferendis, officiis Lorem ipsum dolor sit, amet consectetur
-        adipisicing elit. Numquam magnam unde est maiores voluptas doloribus
-        voluptatum tempore expedita optio a cumque provident officia odit ea
-        recusandae eum quibusdam, maxime cupiditate. Numquam eveniet voluptatum
-        voluptas facilis consectetur, illo nulla cupiditate beatae perferendis,
-        officiis
-      </p>
+      <div className="grid grid-flow-row grid-cols-1 md:grid-cols-3 gap-3">
+        <PostsSkeleton />
+        <PostsSkeleton />
+        <PostsSkeleton />
+      </div>
     </main>
   );
 }
