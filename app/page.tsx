e136531c6f58450716db9889e2f337e95e68ad7c
halmasi <h.almasi2012@gmail.com>
2024-11-15 17:44:37 +0330

    remove home bage bg

--- app/page.tsx
+++ app/page.tsx
@@ -17,7 +17,7 @@ export default async function Home() {
   const data = await getPosts(3);
 
   return (
-    <MainSection>
+    <main className="flex flex-col container max-w-screen-xl py-5 px-10 space-y-2">
       <div className="grid grid-flow-row grid-cols-1 md:grid-cols-3 gap-3">
         {data.map(async (post: PostsProps) => {
           post.categoryUrl = await getCategoriesUrl(post.category);
@@ -36,6 +36,6 @@ export default async function Home() {
           );
         })}
       </div>
-    </MainSection>
+    </main>
   );
 }
