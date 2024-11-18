f9f46564a3396fa4d301af86a2340976abfac701
halmasi <h.almasi2012@gmail.com>
2024-11-18 13:57:33 +0330

    add image to not found page

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,9 +1,16 @@
 import ColoredButton from '@/components/ColoredButton';
-import Link from 'next/link';
-
+import NotFoundImage from '/public/notfound.svg';
+import Image from 'next/image';
 export default function NotFound() {
   return (
-    <div className="flex flex-col pt-20 w-full h-screen items-center bg-gradient-to-br from-yellow-500 via-gray-400 to-pink-400 text-center text-white">
+    <div className="flex flex-col pt-20 w-full min-h-screen items-center bg-gradient-to-br from-yellow-500 via-gray-400 to-pink-400 text-center text-white py-10">
+      <Image
+        className="w-56"
+        src={NotFoundImage.src}
+        width={NotFoundImage.width}
+        height={NotFoundImage.height}
+        alt="Not Found"
+      />
       <h1>صفحه‌ای یافت نشد!</h1>
       <p>متاسفانه صفحه‌ای که دنبال آن هستید وجود ندارد.</p>
       <ColoredButton href="/">بازگشت به صفحه اصلی</ColoredButton>
