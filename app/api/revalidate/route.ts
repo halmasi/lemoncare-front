8949417b33028515b3f54f90e5db9d0f88f9e69b
Hossein Almasi <h.almasi2012@gmail.com>
2024-11-19 20:27:01 +0330

    add revalidation functions WIP

--- app/api/revalidate/route.ts
+++ app/api/revalidate/route.ts
@@ -1,3 +1,9 @@
+import {
+  getCategoriesUrlBySlug,
+  getCategory,
+} from '@/utils/data/getCategories';
+import { getCategoryHierarchy } from '@/utils/data/getPosts';
+import { revalidatePath } from 'next/cache';
 import { NextRequest } from 'next/server';
 
 export async function GET(request: NextRequest) {
@@ -15,47 +21,76 @@ export async function POST(request: NextRequest) {
 
   switch (body.model) {
     case 'post':
-      config = {
-        post: body.entry.basicInfo.contentCode,
-        category: body.entry.category.slug,
-        author: body.entry.author.username,
-      };
+      revalidatePath(`/posts/${body.entry.basicInfo.contentCode}`);
+      const getMainCategory = await getCategory(body.entry.category.slug);
+      const mainCategory = getMainCategory[0];
+      const categoryArray = [getMainCategory[0].slug];
+      const getCategories = await getCategoryHierarchy(
+        mainCategory.parentCategories,
+        'parentCategories'
+      );
+      getCategories.forEach((e) => categoryArray.push(e.slug));
+      categoryArray.map(async (e) => {
+        const url = await getCategoriesUrlBySlug(e);
+        revalidatePath(url, 'layout');
+      });
+      revalidatePath(`/author/${body.entry.author.username}`);
       break;
     case 'author':
-      config = {
-        author: body.entry.name,
-      };
+      revalidatePath(`/author/${body.entry.author.username}`);
       break;
+
     case 'category':
-      config = {
-        category: body.entry.slug,
-      };
+      const getMainCategorybySlug = await getCategory(body.entry.slug);
+      const category = getMainCategorybySlug[0];
+      const categoriesSlugs = [category.slug];
+      const getCategoriesParent = await getCategoryHierarchy(
+        category.parentCategories,
+        'parentCategories'
+      );
+      const getCategoriesChildren = await getCategoryHierarchy(
+        category.parentCategories,
+        'childCategories'
+      );
+      getCategoriesParent.forEach((e) => categoryArray.push(e.slug));
+      getCategoriesChildren.forEach((e) => categoryArray.push(e.slug));
+      categoriesSlugs.map(async (e) => {
+        const url = await getCategoriesUrlBySlug(e);
+        revalidatePath(url, 'layout');
+      });
+      revalidatePath('/posts', 'layout');
       break;
+
     case 'single-page':
       config = {
         singlePost: body.entry.slug,
       };
       break;
+
     case 'tag':
       config = {
         tag: body.entry.slug,
       };
       break;
+
     case 'footer-menu':
       config = {
         footerMenu: body.entry,
       };
       break;
+
     case 'main-menu':
       config = {
         mainMenu: body.entry,
       };
       break;
+
     case 'social-link-menu':
       config = {
         socialLinkMenu: body.entry,
       };
       break;
+
     default:
       console.log(body);
       break;
