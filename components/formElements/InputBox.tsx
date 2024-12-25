f769b658b7c9fa3dcde4a6a72fd8de3348fbca7e
Amin Sadeghi <g.night2000@gmail.com>
2024-12-25 19:28:36 +0330

    remove unnecessary tag in SubmitButton component , install zod , create actionMethod and formValidation for login page

--- components/formElements/InputBox.tsx
+++ components/formElements/InputBox.tsx
@@ -1,14 +1,16 @@
 interface InputProps {
   format?: string;
   placeholder: string;
+  name: string;
 }
 
-export default function InputBox({ format, placeholder }: InputProps) {
+export default function InputBox({ format, placeholder, name }: InputProps) {
   return (
     <input
-      className="p-2 border-2 border-gray-400 rounded-sm shadow-lg focus:shadow-accent-pink/30 focus:outline-none transition-all"
+      className="p-2 border-2 border-gray-400 rounded-xl shadow-lg focus:shadow-accent-pink/30 focus:outline-none transition-all"
       type={format || 'text'}
       placeholder={placeholder}
+      name={name}
     />
   );
 }
