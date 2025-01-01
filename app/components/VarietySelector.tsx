15a5c48a1c2bd87c33d5660b8fba78dafd05407e
Hossein Almasi <h.almasi2012@gmail.com>
2025-01-01 19:31:52 +0330

    fix variety selector

--- app/components/VarietySelector.tsx
+++ app/components/VarietySelector.tsx
@@ -2,22 +2,73 @@
 import { BiShoppingBag } from 'react-icons/bi';
 import { ProductProps } from '../utils/data/getProducts';
 import RadioButton from './formElements/RadioButton';
+import { useEffect, useState } from 'react';
 
 export default function VarietySelector({
   product,
 }: {
   product: ProductProps;
 }) {
+  const [selected, setSelected] = useState<{ id: number; sub: number | null }>({
+    id: 0,
+    sub: null,
+  });
+
+  const itemSelectFunc = ({ id, sub }: { id: number; sub: number | null }) => {
+    setSelected({ id, sub });
+  };
+
+  useEffect(() => {
+    if (product.variety[0].subVariety.length)
+      setSelected({
+        id: product.variety[0].id,
+        sub: product.variety[0].subVariety[0].id,
+      });
+    else setSelected({ id: product.variety[0].id, sub: null });
+  }, []);
+
   return (
     <>
-      <div className="flex gap-2 p-2">
-        {product.variety.map((i, index) => {
-          return (
-            <RadioButton key={index} group="colors" color={i.color}>
-              {i.specification}
-            </RadioButton>
-          );
-        })}
+      <div>
+        <div className="flex gap-2 p-2">
+          {product.variety.map((item, index) => {
+            return (
+              <RadioButton
+                value={{ id: item.id, sub: null }}
+                key={index}
+                group="colors"
+                color={item.color}
+                handler={itemSelectFunc}
+                selectedOptions={selected}
+              >
+                {item.specification}
+              </RadioButton>
+            );
+          })}
+        </div>
+        <>
+          {product.variety.map((item, index) => {
+            return (
+              <div className="flex gap-2" key={index}>
+                {item.subVariety.length > 0 &&
+                  item.subVariety.map((i) => {
+                    return (
+                      <RadioButton
+                        key={i.id}
+                        handler={itemSelectFunc}
+                        selectedOptions={selected}
+                        value={{ id: item.id, sub: i.id }}
+                        color={i.color}
+                        group="subItems"
+                      >
+                        {i.specification}
+                      </RadioButton>
+                    );
+                  })}
+              </div>
+            );
+          })}
+        </>
       </div>
       <button
         onClick={() => {
--- app/components/formElements/RadioButton.tsx
+++ app/components/formElements/RadioButton.tsx
@@ -5,27 +5,42 @@ export default function RadioButton({
   children,
   color,
   group,
+  value,
+  selectedOptions,
+  handler,
 }: {
   children: ReactNode;
   color?: string;
   group: string;
+  value: { id: number; sub: number | null };
+  selectedOptions: { id: number; sub: number | null };
+  handler: ({ id, sub }: { id: number; sub: number | null }) => void;
 }) {
+  const isChecked: boolean = value.sub
+    ? selectedOptions.id == value.id && selectedOptions.sub == value.sub
+    : selectedOptions.id == value.id;
   return (
-    <>
+    <div
+      className={value.sub && selectedOptions.id != value.id ? 'hidden' : ''}
+    >
       <input
-        id={`radio${color}`}
+        id={`radio${value.sub ? `sub${value.sub}` : `parent${value.id}`}`}
         name={group}
-        value={color}
+        value={value.sub || value.id}
         className={`hidden peer`}
         type="radio"
+        checked={isChecked}
+        onChange={() => handler(value)}
       />
       <label
-        className={`flex cursor-pointer border bg-none p-2 rounded-lg peer-checked:border-red-900`}
-        htmlFor={`radio${color}`}
+        className={`flex cursor-pointer border bg-gray-50 p-2 rounded-lg ${isChecked && 'border-red-900 bg-white'}`}
+        htmlFor={`radio${value.sub ? `sub${value.sub}` : `parent${value.id}`}`}
       >
         {children}
-        {color && <FaLemon className="text-2xl" style={{ color }} />}
+        {color != '#000000' && (
+          <FaLemon className="text-2xl" style={{ color }} />
+        )}
       </label>
-    </>
+    </div>
   );
 }
