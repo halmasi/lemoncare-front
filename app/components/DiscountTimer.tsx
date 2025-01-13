75d970fdf25bff0336f1f071e0fe4fbd7e18ca95
Hossein Almasi <h.almasi2012@gmail.com>
2025-01-13 15:50:47 +0330

    revert the tailwind config

--- app/components/DiscountTimer.tsx
+++ app/components/DiscountTimer.tsx
@@ -15,7 +15,7 @@ export default function DiscountTimer({ end }: { end: number }) {
     <div className="flex flex-col items-center pt-2">
       <p>اتمام تخفیف:</p>
       <div className="flex flex-wrap gap-2">
-        <p className="text-sm px-1 rounded-xl border-accent-pink bg-white border-2">
+        <p className="text-sm p-1 rounded-xl border-accent-pink bg-white border-2">
           {end - time > 0
             ? Math.floor((end - time) / (1000 * 60 * 60 * 24))
             : 0}{' '}
@@ -23,7 +23,7 @@ export default function DiscountTimer({ end }: { end: number }) {
         </p>
         <div className="flex gap-2">
           <p className="text-sm">و</p>
-          <p className="text-sm px-1 rounded-xl border-accent-pink bg-white border-2">
+          <p className="text-sm p-1 rounded-xl border-accent-pink bg-white border-2">
             {end - time > 0
               ? Math.floor(
                   ((end - time) % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
@@ -34,7 +34,7 @@ export default function DiscountTimer({ end }: { end: number }) {
         </div>
         <div className="flex gap-2">
           <p className="text-sm">و</p>
-          <p className="text-sm px-1 rounded-xl border-accent-pink bg-white border-2">
+          <p className="text-sm p-1 rounded-xl border-accent-pink bg-white border-2">
             {end - time > 0
               ? Math.floor(((end - time) % (1000 * 60 * 60)) / (1000 * 60))
               : 0}{' '}
@@ -43,7 +43,7 @@ export default function DiscountTimer({ end }: { end: number }) {
         </div>
         <div className="flex gap-2">
           <p className="text-sm">و</p>
-          <p className="text-sm px-1 rounded-xl border-accent-pink bg-white border-2">
+          <p className="text-sm p-1 rounded-xl border-accent-pink bg-white border-2">
             {end - time > 0
               ? Math.floor(((end - time) % (1000 * 60)) / 1000)
               : 0}{' '}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -17,17 +17,6 @@ const config: Config = {
         'accent-green': '#4CAF50',
       },
     },
-    fontSize: {
-      xs: '0.7rem',
-      sm: '0.8rem',
-      base: '1rem',
-      lg: '1.125rem',
-      xl: '1.25rem',
-      '2xl': '1.563rem',
-      '3xl': '1.953rem',
-      '4xl': '2.441rem',
-      '5xl': '3.052rem',
-    },
   },
   plugins: [],
 };
