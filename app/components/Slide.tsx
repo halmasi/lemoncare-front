87f63aad89af47dff4eace76daa7866cc3ab1206
Hossein Almasi <h.almasi2012@gmail.com>
2025-01-19 19:53:42 +0330

    fix some minor issues and implement slides WIP

---
+++ app/components/Slide.tsx
@@ -0,0 +1,13 @@
+'use client';
+
+import { MediaProps } from '../utils/data/getProducts';
+
+interface SlideProps {
+  link: string;
+  slug: string;
+  media: MediaProps;
+}
+
+export default function Slide() {
+  return <div>Slide</div>;
+}
