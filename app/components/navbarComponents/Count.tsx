78b2764fde504c448671a162bc67be9692d1e7e7
halmasi <h.almasi2012@gmail.com>
2025-02-02 18:45:40 +0330

    fix bug in loading suggestions and implementing count component functionality (WIP)

--- app/components/navbarComponents/Count.tsx
+++ app/components/navbarComponents/Count.tsx
@@ -1,23 +1,30 @@
 import { ProductProps } from '@/app/utils/data/getProducts';
 import { useState } from 'react';
 import { BiMinus, BiPlus } from 'react-icons/bi';
+import { RiDeleteBin2Fill } from 'react-icons/ri';
 
 export default function Count({
-  // product,
+  product,
   count,
 }: {
   product: ProductProps;
   count: number;
 }) {
   const [number] = useState(count);
+
   return (
     <div className="flex gap-1 items-center px-5">
-      <button className="p-1 border rounded-full">
-        <BiPlus className="text-lg text-accent-green" />
+      <button
+        disabled={count >= product.variety[0].subVariety[0].inventory}
+        className="p-1 border rounded-full"
+      >
+        <BiPlus
+          className={`text-lg ${count >= 5 ? 'text-gray-300' : 'text-accent-green'}`}
+        />
       </button>
       <p>{number}</p>
-      <button className="p-1 border rounded-full">
-        <BiMinus className="text-lg text-accent-pink" />
+      <button className={`p-1 border rounded-full text-lg text-accent-pink`}>
+        {count <= 1 ? <RiDeleteBin2Fill /> : <BiMinus />}
       </button>
     </div>
   );
