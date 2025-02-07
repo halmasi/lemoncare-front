2c24d0745d06a54a91a9bcad674442477242cbf3
halmasi <h.almasi2012@gmail.com>
2025-02-07 20:05:20 +0330

    implement graphql

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata, Viewport } from 'next';
 import localFont from 'next/font/local';
 import './globals.css';
 import FooterNavbar from './components/FooterNavbar';
+import { ApolloProvider } from '@apollo/client';
+import client from '@/app/utils/apolloClient';
 
 const iranFont = localFont({
   src: './fonts/IRAN.woff',
@@ -28,8 +30,10 @@ export default async function RootLayout({
   return (
     <html lang="fa">
       <body className={`${iranFont.className} antialiased min-h-svh`}>
-        {children}
-        <FooterNavbar />
+        <ApolloProvider client={client}>
+          {children}
+          <FooterNavbar />
+        </ApolloProvider>
       </body>
     </html>
   );
---
+++ app/utils/data/graphql/updateUser.ts
@@ -0,0 +1,19 @@
+import { gql } from '@apollo/client';
+
+export const updateUsersCart = gql`
+  mutation UpdateUsersPermissionsUser(
+    $id: ID!
+    $data: UsersPermissionsUserInput!
+  ) {
+    updateUsersPermissionsUser(id: $id, data: $data) {
+      data {
+        id
+        attributes {
+          fullName
+          username
+          email
+        }
+      }
+    }
+  }
+`;
