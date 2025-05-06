a2b4123da50d2116692c39d8b49959ce79cea295
Hossein Almasi <h.almasi2012@gmail.com>
2025-05-06 19:09:28 +0330

    implementing new search bar

--- app/components/Search.tsx
+++ app/components/Search.tsx
@@ -3,6 +3,8 @@ import { usePathname, useRouter, useSearchParams } from 'next/navigation';
 import { useDebouncedCallback } from 'use-debounce';
 import InputBox from './formElements/InputBox';
 import { useEffect, useRef } from 'react';
+import SubmitButton from './formElements/SubmitButton';
+import { BiSearchAlt2 } from 'react-icons/bi';
 
 export function Search() {
   const inputRef = useRef<HTMLInputElement>(null);
@@ -12,33 +14,39 @@ export function Search() {
 
   useEffect(() => {
     if (inputRef.current) {
-      inputRef.current.value = searchParams.get('query') || '';
+      inputRef.current.value = searchParams.get('s-query') || '';
     }
   }, [searchParams]);
 
   const handleSearch = useDebouncedCallback((term: string) => {
-    console.log(`Searching... ${term}`);
     const params = new URLSearchParams(searchParams);
-    params.set('page', '1');
-
+    params.set('s-page', '1');
     if (term) {
-      params.set('query', term);
-    } else {
-      params.delete('query');
+      params.set('s-query', term);
+      if (inputRef.current) inputRef.current.value = term;
     }
 
     replace(`${pathname}?${params.toString()}`);
   }, 300);
 
   return (
-    <div>
-      <InputBox
-        type="text"
-        placeholder="Search"
-        onChange={(e) => handleSearch(e.target.value)}
-        ref={inputRef}
-        name={'search'}
-      />
+    <div className="flex flex-row justify-stretch w-full">
+      <div className="w-full">
+        <InputBox
+          type="text"
+          placeholder="جستجو مقاله و محصول"
+          onChange={(e) => handleSearch(e.target.value)}
+          ref={inputRef}
+          name={'search'}
+          className="rounded-l-none border-l-0 focus:ring-0 focus:outline-none"
+        />
+      </div>
+      <SubmitButton
+        className="w-fit bg-white hover:bg-gray-50 border rounded-r-none border-r-0 drop-shadow-none text-foreground/80 hover:text-foreground"
+        onClick={() => handleSearch(inputRef.current?.value || '')}
+      >
+        <BiSearchAlt2 />
+      </SubmitButton>
     </div>
   );
 }
