9670b88241fa08e302927e04203feebf04920067
Hossein Almasi <h.almasi2012@gmail.com>
2025-06-12 20:10:55 +0330

    redesign orderHistory page

--- app/(shop)/(profile)/dashboard/orderhistory/page.tsx
+++ app/(shop)/(profile)/dashboard/orderhistory/page.tsx
@@ -11,6 +11,8 @@ import SubmitButton from '@/app/components/formElements/SubmitButton';
 import { useSearchParams } from 'next/navigation';
 import Pagination from '@/app/components/Pagination';
 import { toast } from 'react-toastify';
+import { RiArrowLeftSLine } from 'react-icons/ri';
+import Toman from '@/app/components/Toman';
 
 function LoadingSkeleton() {
   return (
@@ -73,64 +75,109 @@ export default function OrderHistory() {
               <Link
                 href={`/dashboard/orderhistory/${order.orderCode}`}
                 key={order.id}
-                className="gap-2 p-4 bg-white shadow-md hover:shadow-lg rounded-lg border transition-all duration-200 cursor-pointer space-y-2"
+                className="w-full gap-2 p-4 bg-white shadow-md hover:shadow-lg rounded-lg border transition-all duration-200 cursor-pointer space-y-2"
               >
                 {order.items && order.items.length > 0 ? (
-                  <div className="flex flex-col">
-                    <p className="text-sm text-gray-500">
-                      تاریخ:{' '}
-                      {new Date(order.orderDate).toLocaleDateString('fa-IR')}
-                    </p>
-                    <div className="flex flex-row mt-1 gap-3">
-                      {order.items.map((product) => (
-                        <div key={product.id}>
-                          <div className="flex flex-col sm:flex-row gap-3">
-                            <Image
-                              src={
-                                product?.product?.basicInfo?.mainImage?.formats
-                                  ?.thumbnail?.url || '/placeholder.png'
-                              }
-                              alt={
-                                product?.product?.basicInfo?.title || 'بدون نام'
-                              }
-                              width={100}
-                              height={100}
-                              className="rounded-md"
-                            />
-                          </div>
-                          <div>
-                            <p className="font-medium text-gray-700">
-                              {product?.product?.basicInfo?.title || 'بدون نام'}
-                            </p>
+                  <div className="w-full flex flex-col">
+                    <div className="w-full flex justify-between border-b">
+                      <div>
+                        <div
+                          className={`text-sm w-full flex flex-wrap items-center gap-1 p-1`}
+                        >
+                          <div className="flex flex-wrap p-2 gap-2 text-sm">
                             <p className="text-gray-500">
-                              تعداد: {product.count}
+                              {new Date(order.orderDate).toLocaleDateString(
+                                'fa-IR'
+                              )}
                             </p>
+                          </div>
+                          <div className="flex flex-wrap p-2 gap-2 text-sm ">
+                            <p className="text-gray-500">وضعیت پرداخت: </p>
+                            <p
+                              className={`font-semibold ${
+                                order.paymentStatus == 'completed'
+                                  ? 'text-accent-green/75'
+                                  : 'text-accent-pink/75'
+                              }`}
+                            >
+                              {order.paymentStatus == 'completed'
+                                ? 'پرداخت شده'
+                                : order.paymentStatus == 'pending'
+                                  ? 'در انتظار پرداخت'
+                                  : 'لغو شده'}
+                            </p>
+                          </div>
+                          <div className="flex flex-wrap p-2 gap-2 text-sm ">
+                            <p className="text-gray-500">شناسه سفارش: </p>
+                            <p>{order.orderCode}</p>
                           </div>
                         </div>
-                      ))}
-                    </div>
-                    <div
-                      className={`text-sm w-full flex flex-wrap justify-between items-center gap-2`}
-                    >
-                      <p>
-                        <span className="text-gray-500">وضعیت پرداخت: </span>
-                        <span
-                          className={`font-semibold ${
-                            order.paymentStatus == 'completed'
-                              ? 'text-accent-green/75'
-                              : 'text-accent-pink/75'
-                          }`}
+                        <div
+                          className={`text-sm w-full flex flex-wrap items-center gap-1 p-1`}
                         >
-                          {order.paymentStatus == 'completed'
-                            ? 'پرداخت شده'
-                            : order.paymentStatus == 'pending'
-                              ? 'در انتظار پرداخت'
-                              : 'لغو شده'}
-                        </span>
-                      </p>
-                      <div className="self-end w-fit">
-                        <SubmitButton>مشاهده سفارش</SubmitButton>
+                          <div className="flex flex-wrap p-2 gap-2 text-sm ">
+                            <p className="text-gray-500">مبلغ: </p>
+                            <Toman className="fill-accent-green text-accent-green">
+                              <p>
+                                {(order.totalPrice / 10).toLocaleString(
+                                  'fa-IR'
+                                )}
+                              </p>
+                            </Toman>
+                          </div>
+                          <div className="flex flex-wrap p-2 gap-2 text-sm ">
+                            <p className="text-gray-500">تخفیف: </p>
+                            <Toman className="fill-accent-green text-accent-green">
+                              <p>
+                                {(() => {
+                                  let offer = 0;
+                                  order.items.forEach((item) => {
+                                    if (item.beforePrice && item.mainPrice)
+                                      offer +=
+                                        item.beforePrice - item.mainPrice;
+                                  });
+                                  return (
+                                    <span>
+                                      {(
+                                        (order.orderPrice - offer) /
+                                        10
+                                      ).toLocaleString('fa-IR')}
+                                    </span>
+                                  );
+                                })()}
+                              </p>
+                            </Toman>
+                          </div>
+                        </div>
                       </div>
+                      <p className="self-center p-2 w-fit hover:text-accent-pink transition-colors">
+                        <RiArrowLeftSLine />
+                      </p>
+                    </div>
+                    <div className="w-full flex flex-row mt-1 gap-3 p-1">
+                      {(() => {
+                        const i = order.items.slice(0, 4);
+                        return i.map((product, index) => (
+                          <Image
+                            key={product.id}
+                            src={
+                              product?.product?.basicInfo?.mainImage?.formats
+                                ?.thumbnail?.url || '/placeholder.png'
+                            }
+                            alt={
+                              product?.product?.basicInfo?.title || 'بدون نام'
+                            }
+                            width={100}
+                            height={100}
+                            className={`w-16 h-26 rounded-full aspect-square object-cover border-background border-2 ${index > 0 && '-mr-8'}`}
+                          />
+                        ));
+                      })()}
+                      {order.items.length > 4 && (
+                        <div className="flex items-center justify-center w-16 h-16 rounded-full aspect-square object-cover border-2 bg-background/85 -mr-8">
+                          <p>{order.items.length - 4}+</p>
+                        </div>
+                      )}
                     </div>
                   </div>
                 ) : (
